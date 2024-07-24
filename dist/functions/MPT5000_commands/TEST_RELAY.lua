1d85d207df09455170ef8d5fb5f05740257859f2
Vlad Feldfix <62237119+VladFeldfix@users.noreply.github.com>
2024-07-24 10:31:13 +0300

    Add files via upload

--- dist/functions/MPT5000_commands/TEST_RELAY.lua
+++ dist/functions/MPT5000_commands/TEST_RELAY.lua
@@ -2,8 +2,6 @@
 SetHighAuxiliary(INPUT_PLUS)
 SetLowAuxiliary(INPUT_MINUS)
 SetPPSOn('hcs', {i = 0.02, v = 24})
-Sleep(300)
-RelaySetup = {i = 0.1, tare = {mode = 'fixed', data = {res = 2.5}}}
-WaitForContinuity("Wait for continuity", "Testing relay RELAYNAME", OUTPUT_PLUS, OUTPUT_MINUS, 'msr', RelaySetup, 1)
-ClearAllPoints()
+WaitForContinuity("Wait for continuity", "Testing relay RELAYNAME", OUTPUT_PLUS, OUTPUT_MINUS, 'msr', {i = 100 mA, delay = 100 ms,  tare = {mode='fixed', data = {res = 2 Ohm}}}, 1)
 SetPPSOff('hcs')
+ClearAllPoints()
