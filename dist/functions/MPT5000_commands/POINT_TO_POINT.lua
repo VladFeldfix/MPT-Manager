78d8d906da43ef1097b5f02300800e5aa89c1b45
Vlad Feldfix <62237119+VladFeldfix@users.noreply.github.com>
2024-08-20 11:16:31 +0300

    Add files via upload

--- dist/functions/MPT5000_commands/POINT_TO_POINT.lua
+++ dist/functions/MPT5000_commands/POINT_TO_POINT.lua
@@ -1,6 +1,6 @@
 -- Point to point test
 Report.Info("")
 Report.Info("Testing contact from POINT1 to POINT2")
-WaitForContinuity("Testing continuity POINT1 -> POINT2", "Contact Point: PROBE to Point: POINT1", PROBE, POINT2, 'msr', {i = 100 mA, delay = 100ms,  tare = {mode='fixed', data = {res = 2.20Ohm}}},1.6, 1000)
+WaitForContinuity("Testing continuity POINT1 -> POINT2", "Contact Point: PROBE to Point: POINT1", PROBE, POINT2, 'msr', {i = 100 mA,  tare = {mode='fixed', data = {res = 2 Ohm}}},1)
 Beep(100)
 ClearAllPoints()
