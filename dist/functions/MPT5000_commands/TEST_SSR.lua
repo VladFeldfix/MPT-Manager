c1265d180f3dec7d40329353b993569f202efbed
Vlad Feldfix <62237119+VladFeldfix@users.noreply.github.com>
2024-09-24 11:07:52 +0300

    Delete dist directory

--- dist/functions/MPT5000_commands/TEST_SSR.lua
+++
@@ -1,9 +0,0 @@
--- Test SSR
-SetHighAuxiliary(INPUT3)
-SetLow(AuxiliaryINPUT4)
-SetPPSOn('hcs', {i = 0.02, v = 24})
-Sleep(300)
-SSRSetup = {i = 0.1, tare = {mode = 'fixed', data = {res = 2.5}}}
-WaitForContinuity("Wait for continuity", "Testing SSR SSRNAME", OUTPUT1, OUTPUT2, 'msr', SSRSetup, 1)
-ClearAllPoints()
-SetPPSOff('hcs')
