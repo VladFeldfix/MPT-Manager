0413d711a7ee7a4dd5242652b12046f8ab6605a0
Vlad Feldfix <62237119+VladFeldfix@users.noreply.github.com>
2024-09-24 16:00:16 +0300

    Add files via upload

--- functions/outlets.py
+++ functions/outlets.py
@@ -2,27 +2,54 @@ Outlets = {}
 Outlets["A1"] = 0
 Outlets["B1"] = 50
 Outlets["C1"] = 100
+
 Outlets["A2"] = 150
 Outlets["B2"] = 200
 Outlets["C2"] = 250
+
 Outlets["A3"] = 300
 Outlets["B3"] = 350
 Outlets["C3"] = 400
+
 Outlets["A4"] = 450
 Outlets["B4"] = 500
 Outlets["C4"] = 550
+
 Outlets["A5"] = 600
 Outlets["B5"] = 650
 Outlets["C5"] = 700
+
 Outlets["A6"] = 750
 Outlets["B6"] = 800
 Outlets["C6"] = 850
+
 Outlets["A7"] = 900
 Outlets["B7"] = 950
 Outlets["C7"] = 1000
+
 Outlets["A8"] = 1050
 Outlets["B8"] = 1100
 Outlets["C8"] = 1150
+
+Outlets["A9"] = 1200
+Outlets["B9"] = 1250
+Outlets["C9"] = 1300
+
+Outlets["A10"] = 1350
+Outlets["B10"] = 1400
+Outlets["C10"] = 1450
+
+Outlets["A11"] = 1500
+Outlets["B11"] = 1550
+Outlets["C11"] = 1600
+
+Outlets["A12"] = 1650
+Outlets["B12"] = 1700
+Outlets["C12"] = 1750
+
+Outlets["A13"] = 1800
+Outlets["B13"] = 1850
+Outlets["C13"] = 1900
 
 def GetOutletStart(out):
     return Outlets[out]
