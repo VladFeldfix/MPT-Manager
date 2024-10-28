4d4edc9bb8b12cbb7673baa24941c67464362a7a
Vlad Feldfix <62237119+VladFeldfix@users.noreply.github.com>
2024-10-29 00:28:36 +0200

    general sync

--- functions/MPT5000_commands/TEST_INSULATION.lua
+++
@@ -1,8 +0,0 @@
--- Isolation Test
-Report.Info("")
-IsolationSetup = {
-  setup = {i = 10 uA, tare = {mode = 'system'}},
-  criteria = {r > 100 kOhm},
-  diagnose = {},
-}
-Isolation('Test Isolation', 'msr', IsolationSetup, 'all')
--- functions/MPT5000_commands/TEST_RELAY.lua
+++
@@ -1,7 +0,0 @@
--- Test relay
-SetHighAuxiliary(INPUT_PLUS)
-SetLowAuxiliary(INPUT_MINUS)
-SetPPSOn('hcs', {i = 0.02, v = 24})
-WaitForContinuity("Wait for continuity between OUTPUT_PLUS - OUTPUT_MINUS", "Testing relay RELAYNAME", OUTPUT_PLUS, OUTPUT_MINUS, 'msr', {i = 100 mA, delay = 100 ms,  tare = {mode='fixed', data = {res = 2 Ohm}}}, 1)
-SetPPSOff('hcs')
-ClearAllPoints()
